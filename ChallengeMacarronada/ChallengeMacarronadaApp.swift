8d31fc3fb253f25f0a6702a2f6a17034d7fb6215
thaxz <thaxzt@gmail.com>
2023-05-23 11:49:46 -0300

    first commit

---
+++ ChallengeMacarronada/ChallengeMacarronadaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeMacarronadaApp.swift
+//  ChallengeMacarronada
+//
+//  Created by thaxz on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeMacarronadaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
