f77bcf310341433f5ca6023187ac8116204327fa
thaxz <thaxzt@gmail.com>
2023-07-05 20:17:33 -0300

    hotfix(AppStore): Finishing onboardingView

--- ChallengeMacarronada/Core/Onboarding/Views/OnboardingView.swift
+++ ChallengeMacarronada/Core/Onboarding/Views/OnboardingView.swift
@@ -20,16 +20,39 @@ struct OnboardingView: View {
                         .frame(width: 32, height: 32)
                         .foregroundColor(Color.theme.text)
                 }
-                
-                Image(systemName: "ellipsis.circle")
+                Image("bellBird")
                     .resizable()
                     .frame(width: 150, height: 150)
+                Spacer()
+                    .frame(height: 30)
+                Text("Seja bem vindo a Bell")
+                    .font(.system(size: 22, weight: .semibold))
+                    .foregroundColor(Color.theme.text)
+                Text("Bell divide o dia em turnos através de uma interface prática e manda notificações ao longo do dia para simplificar o seu processo de organização.")
+                    .font(.system(size: 14, weight: .regular))
+                    .foregroundColor(Color.theme.text)
+                    .multilineTextAlignment(.center)
+                Spacer()
+                    .frame(height: 30)
+                Button {
                     
+                } label: {
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.theme.evening)
+                        Text("Começar a usar Bell")
+                            .font(.system(size: 16, weight: .bold))
+                            .foregroundColor(Color.theme.text)
+                    }
+                    .frame(width: 220, height: 40)
+                } .buttonStyle(PlainButtonStyle())
+
                 
                 Spacer()
             }
-            .padding(24)
+            .padding(32)
         }
+        .frame(width: 390, height: 624)
     }
 }
 
